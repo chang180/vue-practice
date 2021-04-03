fb9cacab9fff5e49bd533d63a9bb68bc226b1692
張建文 <chang180@gmail.com>
2021-04-03 15:46:52 +0800

    not working properly

--- src/main.js
+++ src/main.js
@@ -5,6 +5,14 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+Vue.directive('focus', {
+  inserted: (el, binding, vnode, oldVnode) => {
+    // console.log(this)
+    // console.log(el, binding, vnode, oldVnode)
+    el.focus()
+  }
+})
+
 new Vue({
   router,
   store,
--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -9,16 +9,40 @@ space-before-blocks */
     <router-link :to="{ name: 'Todo', query: { filter: 'done' } }" replace
       >Done </router-link
     >
-    <p>show:{{ filter }}</p>
-    <div>{{ list }}</div>
+    <!-- <p>show:{{ filter }}</p>
+    <div>{{ list }}</div> -->
+    <ul>
+      <!-- <li v-for="item of list" :key="item.tId">
+        <template v-if="edit!==item.tId">
+        <input type="checkbox" v-model="item.todo.done" :key="item.tId">{{ item.todo.content }}
+        </template>
+        <template v-else>
+        <input type="text" v-model="item.todo.content" :key="item.tId">
+        </template>
+      </li> -->
+      <TodoItem
+      v-for="item of list"
+      :key="item.tId+''+item.todo.content"
+      :todo="item.todo"
+      :edit="item.tId===edit"
+      @check="value => checkHandler(item.tId, value)"
+      @editThis ="edit = item.tId"
+      @editComplete ="value => editCompleteHandler(item.tId, value)"
+      />
+    </ul>
   </div>
 </template>
 <script>
+import TodoItem from '../components/TodoItem'
 export default {
   data () {
     return {
-      filter: 'all' // all,active,done
+      filter: 'all', // all,active,done
+      edit: null
     }
+  },
+  mounted () {
+    this.$store.dispatch('READ_TODOS')
   },
   computed: {
     list () {
@@ -33,10 +57,26 @@ export default {
     $route: {
       immediate: true,
       handler: function (route) {
+        this.edit = null
         // check query => redirect
         this.filter = route.query.filter || 'all'
       }
     }
+  },
+  methods: {
+    checkHandler (tId, done) {
+      // console.log(tId)
+      // console.log(tId, done)
+      this.$store.dispatch('CHECK_TODO', { tId, done })
+    },
+    editCompleteHandler (tId, content) {
+      this.edit = null
+      this.$store.dispatch('UPDATE_TODO', { tId, content })
+      // console.log(tId, content)
+    }
+  },
+  components: {
+    TodoItem
   }
 }
 </script>
